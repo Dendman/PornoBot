72f5e457a8fad29a29992b39a92ac5dae38d0260
denis <denddman@gmail.com>
2022-08-05 19:26:57 +0300

    Нормальное состояние бота рабочее (присылает анимешки)

---
+++ src/main/java/io/proj3ct/PornoBot/service/TelegramBot.java
@@ -0,0 +1,119 @@
+package io.proj3ct.PornoBot.service;
+
+import io.proj3ct.PornoBot.config.BotConfig;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScope;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Slf4j
+@Component
+
+public class TelegramBot extends TelegramLongPollingBot {
+
+    final BotConfig config;
+    static final int picture = 0;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+        List<BotCommand> listofCommands = new ArrayList<>();
+        listofCommands.add(new BotCommand("/start", "get hello message"));
+        listofCommands.add(new BotCommand("/anime", "get anime hentai"));
+        listofCommands.add(new BotCommand("/tride", "get tride porn"));
+        try {
+            this.execute(new SetMyCommands(listofCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error setting bot's command list: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @SneakyThrows
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    sendMessage(chatId, "Пока не работает:(");
+                    break;
+                case "/anime":
+                    sendImageUploadingAFile(chatId);
+                    break;
+            }
+
+        }
+
+    }
+
+    private void startCommandReceived(long chatId, String name) {
+        String answer = "Привет, " + name + ", я порнобот. Какую порнушку хочешь?";
+        sendMessage(chatId, answer);
+        log.info("Replied to user" + name);
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void sendImageUploadingAFile(Long chatId) {
+        // Create send method
+        SendPhoto sendPhotoRequest = new SendPhoto();
+        // Set destination chat id
+        sendPhotoRequest.setChatId(String.valueOf(chatId));
+        // Set the photo file as a new photo (You can also use InputStream with a constructor overload)
+        Random rand = new Random();
+        File[] files = new File( "/home/denis/Загрузки/PornoBot/src/main/resources/anime" ).listFiles();
+        File file = files[ rand.nextInt( files.length ) ];
+        sendPhotoRequest.setPhoto(new InputFile(new File(file.getAbsolutePath())));
+        try {
+            // Execute the method
+            execute(sendPhotoRequest);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
